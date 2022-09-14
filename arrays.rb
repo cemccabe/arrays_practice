8e44c89e2ae1d9f09ddca255c9b927e7b930ad4c
Christian McCabe <christian.mccabe89@gmail.com>
2022-09-14 09:22:23 -0600

    Call 4 array methods with comments

--- arrays.rb
+++ arrays.rb
@@ -2,3 +2,19 @@ dog_names = ["Sampson", "Rebel", "Lady", "Delta"]
 dog_ages = [5, 6, 10, 3]
 food_costs = [21.21, 50.44, 37.76, 39.90]
 vet_appointments = [true, true, false, false]
+
+# pop method will remove the last element the array
+# dog_names.pop will remove element "Delta"
+dog_names.pop
+
+# push method will add an element to the end of the array
+# dog_ages.push(12) will add int 12 to index 4 of array
+dog_ages.push(12)
+
+# shift method will remove the first element of the array
+# food_costs.shift will remove element 21.21
+food_costs.shift
+
+# unshift method will prepend an element to array
+# vet_appointments.unshift(false) will add bool false to index 0 of array
+vet_appointments.unshift(false)
